5432a5c98abcf0e2dc24443dff641b2fddc99869
Grzegorz <grzegorzf@hotmail.com>
2018-09-07 21:03:30 +0200

    Update 001.py

--- 001.py
+++ 001.py
@@ -1,4 +1,4 @@
-userName = input("Podaj imię: ")
-userAge = int(input("Podaj wiek: "))
+userName = input("Your name is: ")
+userAge = int(input("Your age is: "))
 century = (2018-userAge+100)
-print(userName, " sto lat będziesz miał w", century)
+print(userName, " your hundert birthday is in ", century,"y")
