c3acbaf863be4082955088b89f7bd85bf4802840
Grzegorz <grzegorzf@hotmail.com>
2018-09-08 12:03:13 +0200

    Add files via upload

--- 003.py
+++ 003.py
@@ -1,8 +1,8 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = []
-for element in a:
-    if element<5:
+b = [] #extras no. 1
+userNumber = int(input("Give me a number 1 - 89: "))
+for element in range(0, len(a), 1):
+    if a[element] < userNumber:
         b.append(a[element])
-
-for element in b:
+for element in range(0, len(b), 1):
     print(b[element])
