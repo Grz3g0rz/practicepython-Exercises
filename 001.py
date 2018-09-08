61608ec4a9c74a1055726c9c4b645c728ec23d0b
Grzegorz <grzegorzf@hotmail.com>
2018-09-08 17:19:07 +0200

    Add files via upload

--- 001.py
+++ 001.py
@@ -1,3 +1,13 @@
+"""
+Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+Extras:
+
+1. Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. (Hint: order of operations exists in Python)
+2. Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as pressing the ENTER button
+"""
+
+
 userName = input("Your name is: ")
 userAge = int(input("Your age is: "))
 century = (2018-userAge+100)
--- 004.py
+++ 004.py
@@ -1,3 +1,5 @@
+#Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
+
 userNumber = int(input("Input number: "))
 x = range(1, userNumber+1)
 divisor = []
