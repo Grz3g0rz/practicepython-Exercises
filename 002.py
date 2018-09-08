f4842f0d58f9403f8bc41c59bded47ca9e4b7de0
Grzegorz <grzegorzf@hotmail.com>
2018-09-08 11:54:27 +0200

    Add files via upload

--- 002.py
+++ 002.py
@@ -5,3 +5,9 @@ elif userNumber%2 == 0:
     print("Even, div by 2 mod 0")
 else:
     print("Odd")
+
+check = int(input("Give another number: "))
+if userNumber%check == 0:
+    print("Divides evenly")
+else:
+    print("Divides NOT evenly!")
