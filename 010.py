9f6d2084d3bee3656f2c59428696a9a4c2be833b
Grzegorz <82grzegorz@gmail.com>
2018-09-22 19:43:34 +0200

    Removed some things

--- 010.py
+++ 010.py
@@ -1,6 +1,12 @@
-omly generate two lists to test this
 """
-#nowsza wersja pliku po zmianach
+write a program that returns a list that contains only the elements that are common between the lists (without duplicates).
+Make sure your program works on two lists of different sizes. Write this in one line of Python using at least one list comprehension.
+
+Extra:
+
+Randomly generate two lists to test this
+"""
+
 import random
 firstList = []
 secondList = []
