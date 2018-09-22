c84608516d57074f174bc9d401e666e3db6028c0
Grzegorz <82grzegorz@gmail.com>
2018-09-22 19:40:05 +0200

    Nauka gita, wrzucenie do repo

--- 010.py
+++ 010.py
@@ -1,12 +1,6 @@
+omly generate two lists to test this
 """
-write a program that returns a list that contains only the elements that are common between the lists (without duplicates).
-Make sure your program works on two lists of different sizes. Write this in one line of Python using at least one list comprehension.
-
-Extra:
-
-Randomly generate two lists to test this
-"""
-
+#nowsza wersja pliku po zmianach
 import random
 firstList = []
 secondList = []
